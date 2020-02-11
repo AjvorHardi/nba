af078f2155d26811867a7738d8f0bb1f8c44f268
AjvorHardi <markovic.goran@outlook.com>
2020-02-11 16:07:52 +0100

    Zadatak 4

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -7,6 +7,12 @@ use App\Team;
 
 class TeamController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index() {
         $teams = Team::all();
         return view('teams', compact('teams'));
